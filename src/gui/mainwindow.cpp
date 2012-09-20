fd0267fd4b4110ec3ca585dc374d5bbcf62a39b8
Jacques de Laval <chucky@wrutschkow.org>
2012-09-20 15:01:40 +0200

    Bajs

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -20,12 +20,17 @@
 #include <QRegExp>
 #include <QClipboard>
 
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
 
     qDebug() << qgetenv("PATH");
+    QMessageBox msgBox;
+    msgBox.setText(QProcessEnvironment::systemEnvironment().value("PATH"));
+     msgBox.exec();
 
     qnam = new PirateNetworkAccessManager(this);
     downloadStack = new DownloadListModel(this, qnam);
